e89b2ac527941a309024f7550d07d261f1b995fb
mac <mac@mFelipe-MacBook-Air.local>
2022-06-05 15:36:47 +0100

    Estrutura de Rotas do App.tsx para Importar os Componentes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { NativeBaseProvider, Box } from "native-base";
+import { NativeBaseProvider } from "native-base";
+import { SignIn } from "./src/screens/SignIn";
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Box>Hello world</Box>
+      <SignIn />
     </NativeBaseProvider>
   );
 }
